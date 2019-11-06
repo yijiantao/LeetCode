9d9846a15d1b26fd50a7d823632832ab790f6c0c
yijiantao <yijiantao@hotmail.com>
2019-11-06 22:33:21 +0800

    阅读笔记

--- LeetCode Algorithms/189.旋转数组.py
+++ LeetCode Algorithms/189.旋转数组.py
@@ -10,6 +10,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # nums[:] 是深复制 ，在python的赋值中，是通过对象的地址引用进行的赋值，nums[:]修改的是堆中的内容，意思是指针还指向本身
+        # nums = 是浅拷贝，修改nums就新开辟了一个地址空间, 测试代码为: 赋值后用 id(a) 与 id(a[:])查看地址
         nums[:] = nums[len(nums) - k:] + nums[:len(nums) - k]
         return nums
         
