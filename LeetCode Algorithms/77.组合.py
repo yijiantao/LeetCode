62d82e3f74de9c348db594fcfbf1f4bc7a2ca633
yijiantao <yijiantao@hotmail.com>
2019-11-23 00:04:50 +0800

    :tada: LeetCode代码同步

--- LeetCode Algorithms/77.组合.py
+++ LeetCode Algorithms/77.组合.py
@@ -7,6 +7,7 @@
 # @lc code=start
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        
+        import itertools
+        return list(itertools.combinations(range(1, n + 1), k))
 # @lc code=end
 
