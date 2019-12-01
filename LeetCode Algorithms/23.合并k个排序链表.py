bf61d16fa7dd345745dfaa5351f05438123e121a
yijiantao <yijiantao@hotmail.com>
2019-12-01 22:53:27 +0800

    :cry: 未完待做

---
+++ LeetCode Algorithms/23.合并k个排序链表.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode.cn id=23 lang=python3
+#
+# [23] 合并K个排序链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, x):
+# #         self.val = x
+# #         self.next = None
+
+# class Solution:
+#     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+#         result_link = lists[]
+#         for link_index in lists:
+
+# @lc code=end
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+# class Solution:
+#     @staticmethod
+#     def mergeKLists(lists):
+#         for link_index in lists:
+
+# lists = []
+# print (Solution.mergeKLists(lists))
+
+print ()
