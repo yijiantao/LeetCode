4703ab78941c193fe26d85ca83dfa19292db2867
yijiantao <yijiantao@hotmail.com>
2019-12-11 21:37:05 +0800

    :tada: 更新代码+ReadMe文档

---
+++ LeetCode Algorithms/17.电话号码的字母组合.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=17 lang=python3
+#
+# [17] 电话号码的字母组合
+#
+
+# @lc code=start
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        phone = {'2': ['a', 'b', 'c'],
+                 '3': ['d', 'e', 'f'],
+                 '4': ['g', 'h', 'i'],
+                 '5': ['j', 'k', 'l'],
+                 '6': ['m', 'n', 'o'],
+                 '7': ['p', 'q', 'r', 's'],
+                 '8': ['t', 'u', 'v'],
+                 '9': ['w', 'x', 'y', 'z']}
+        if not digits: return []
+        res = ['']
+        for _index in digits:
+            res = [x + y for x in res for y in phone[_index]]
+        return res
+# @lc code=end
