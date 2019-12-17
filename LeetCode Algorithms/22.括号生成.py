abf503b7d32053151c382abfa391e120c616caab
yijiantao <yijiantao@hotmail.com>
2019-12-17 22:34:59 +0800

    提交代码

---
+++ LeetCode Algorithms/22.括号生成.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=22 lang=python3
+#
+# [22] 括号生成
+#
+
+# @lc code=start
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def backtrack(S = '', left = 0, right = 0):
+            if len(S) == 2 * n:
+                res.append(S)
+                return
+            if left < n:
+                backtrack(S+'(', left + 1, right)
+            if right < left:    # 如果不超过左括号的数量，就可以放一个右括号。
+                backtrack(S+')', left, right + 1)
+        backtrack()
+        return res
+# @lc code=end
+
