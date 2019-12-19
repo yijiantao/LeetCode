47a3cc159e2f6179b85b8626aab16353097e27eb
yijiantao <yijiantao@hotmail.com>
2019-12-20 00:37:49 +0800

    :tada: 更新刷题代码+说明文档(动态规划算法)

--- LeetCode Algorithms/70.爬楼梯.py
+++ LeetCode Algorithms/70.爬楼梯.py
@@ -10,8 +10,13 @@ class Solution:
     @classmethod
     def climbStairs(self, n):
         if n <= 2: return n
-        # 斐波拉契数列
-        
+        # 斐波拉契数列 + 动态规划算法
+        dp = [0] * (n + 1)
+        dp[1], dp[2] = 1, 2
+        for _index in range(3, n + 1):
+            dp[_index] = dp[_index - 1] + dp[_index - 2]
+        return dp[n]
+
 # @lc code=end
 
-print (Solution.climbStairs(2))
+print (Solution.climbStairs(33))
