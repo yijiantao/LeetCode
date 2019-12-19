1c8ef40b8735ef6927135d2a8b774c81d0d0c3e0
yijiantao <yijiantao@hotmail.com>
2019-12-20 00:08:21 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/66.加一.py
+++ LeetCode Algorithms/66.加一.py
@@ -10,5 +10,13 @@ class Solution:
         # 方法一：
         return [int(_index) for _index in str(int(''.join([str(_index) for _index in digits])) + 1)]
         # 方法二:
-
+        if not digits: return [0]
+        jinwei_flag = 0
+        digits[-1] += 1
+        for _index in range(len(digits) - 1, -1, -1):
+            digits[_index] += jinwei_flag
+            jinwei_flag = digits[_index] // 10
+            digits[_index] %= 10
+        if jinwei_flag != 0: digits.insert(0, jinwei_flag)
+        return digits
 # @lc code=end
