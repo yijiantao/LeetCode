a1a2f26ad854814dfa41957fec1173c138d05a1e
yijiantao <yijiantao@hotmail.com>
2019-12-22 19:48:43 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/53.最大子序和.py
+++ LeetCode Algorithms/53.最大子序和.py
@@ -8,7 +8,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         # 动态规划 DP
-        
+        # 分析存在如下两种情况：
+        ## 1.当前元素为子序起始点
+        ## 2.当前元素为子序中间或结尾点
+        ## 因此：nums[i] = nums[i]和nums[i] + nums[i -1]（第二种情况）中的最大值
+
         max_num = nums[0]
         for _index in range(1, len(nums)):
             nums[_index] = max(nums[_index], nums[_index] + nums[_index - 1])
