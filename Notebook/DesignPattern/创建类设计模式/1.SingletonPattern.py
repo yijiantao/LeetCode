acadd519a8f654e4db803686b33eaf24cf49396b
yijiantao <yijiantao@hotmail.com>
2019-12-23 23:03:54 +0800

    :tada: 单例模式.py

--- Notebook/DesignPattern/创建类设计模式/1.SingletonPattern.py
+++ Notebook/DesignPattern/创建类设计模式/1.SingletonPattern.py
@@ -10,36 +10,43 @@
 
 import threading
 import time
-#这里使用方法__new__来实现单例模式
-class Singleton(object):#抽象单例
+
+# 这里使用方法__new__来实现单例模式
+class Singleton(object):    # 抽象单例
     def __new__(cls, *args, **kw):
         if not hasattr(cls, '_instance'):
             orig = super(Singleton, cls)
             cls._instance = orig.__new__(cls, *args, **kw)
         return cls._instance
-#总线
+
+# 总线对象
 class Bus(Singleton):
     lock = threading.RLock()
-    def sendData(self,data):
+
+    def sendData(self, data):
         self.lock.acquire()
         time.sleep(3)
-        print ("Sending Signal Data...", data)
+        print("Sending Signal Data...", data)
         self.lock.release()
-#线程对象，为更加说明单例的含义，这里将Bus对象实例化写在了run里
+
+# 线程对象，为更加说明单例的含义，这里将Bus对象实例化写在了run里
 class VisitEntity(threading.Thread):
-    my_bus=""
-    name=""
+    my_bus = ""
+    name = ""
+
     def getName(self):
         return self.name
+
     def setName(self, name):
-        self.name=name
+        self.name = name
+
     def run(self):
-        self.my_bus=Bus()
+        self.my_bus = Bus()
         self.my_bus.sendData(self.name)
 
-if  __name__=="__main__":
+if __name__ == "__main__":
     for _index in range(3):
-        print (f"Entity {_index} begin to run...")
-        my_entity=VisitEntity()
+        print(f"Entity {_index} begin to run...")
+        my_entity = VisitEntity()
         my_entity.setName("Entity_"+str(_index))
         my_entity.start()
