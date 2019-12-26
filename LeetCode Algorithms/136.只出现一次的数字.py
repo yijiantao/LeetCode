523f6c323fde3d4ac58bbe5c16c9cd58c4c9732f
yijiantao <yijiantao@hotmail.com>
2019-12-26 22:48:41 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/136.只出现一次的数字.py
+++ LeetCode Algorithms/136.只出现一次的数字.py
@@ -14,7 +14,18 @@ class Solution:
 
         # 方法二
         # 除了某个元素只出现一次以外，其余每个元素均出现两次。
-        return 2 * sum(set(nums)) - sum(nums)
+        # return 2 * sum(set(nums)) - sum(nums)
+
+        # 方法三： 异或运算！
+        ## 如果我们对 0 和二进制位做 XOR 运算，得到的仍然是这个二进制位
+        ## a ^ 0 = a
+        ##如果我们对相同的二进制位做 XOR 运算，返回的结果是 0
+        ## a ^ a = 0
+        ## 所以我们只需要将所有的数进行 XOR 操作，得到那个唯一的数字。
+        a = 0
+        for i in nums:
+            a ^= i
+        return a
 
 # @lc code=end
 
