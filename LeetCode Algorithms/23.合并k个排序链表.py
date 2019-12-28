9384f49ae2e7cbde33f0f7004a66c73da93cbada
yijiantao <yijiantao@hotmail.com>
2019-12-28 22:13:28 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/23.合并k个排序链表.py
+++ LeetCode Algorithms/23.合并k个排序链表.py
@@ -6,29 +6,25 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-# # class ListNode:
-# #     def __init__(self, x):
-# #         self.val = x
-# #         self.next = None
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-# class Solution:
-#     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-#         result_link = lists[]
-#         for link_index in lists:
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        # 遍历所有链表，将所有节点的值放到一个数组中。
+        self.node = []
+        head = point = ListNode(0)
+        for link_index in lists:
+            while link_index:
+                self.node.append(link_index.val)
+                link_index = link_index.next
+        for node_index in sorted(self.node):
+            point.next = ListNode(node_index)
+            point = point.next
+        return head.next
 
 # @lc code=end
-
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-# class Solution:
-#     @staticmethod
-#     def mergeKLists(lists):
-#         for link_index in lists:
-
-# lists = []
-# print (Solution.mergeKLists(lists))
 
 print ()
