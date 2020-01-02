5e2d0574614f5a0767bbe2dc77c0610d6485d0c3
yijiantao <yijiantao@hotmail.com>
2020-01-03 00:21:00 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/191.位-1-的个数.py
+++ LeetCode Algorithms/191.位-1-的个数.py
@@ -6,6 +6,11 @@
 
 # @lc code=start
 class Solution:
-    def hammingWeight(self, n: int) -> int:
+    # def hammingWeight(self, n: int) -> int:
+    @classmethod
+    def hammingWeight(self, n):
+        # python 内置 bin() 函数
+        ##  输入： int 或者 long int 数字
+        ##  输出：字符串
         return bin(n).count('1')
 # @lc code=end
