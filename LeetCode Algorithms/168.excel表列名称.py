aa808332c6bf4c2052ed7a8dad07fc5ec4d5b812
yijiantao <yijiantao@hotmail.com>
2020-01-05 17:00:41 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/168.excel表列名称.py
+++ LeetCode Algorithms/168.excel表列名称.py
@@ -9,10 +9,13 @@ class Solution:
     def convertToTitle(self, n: int) -> str:
         res = ''
         while n:
+            # divmod()函数把除数和余数运算结果结合起来；
+            # divmod(a, b)返回一个包含商和余数的元组(a // b, a % b)
             n, temp = divmod(n, 26)
             if temp == 0:
                 n -= 1
                 temp = 26
+            # chr() )函数是输入一个整数【0，255】返回其对应的ascii码
             res = chr(temp + 64) + res
         return res
 # @lc code=end
