4143e133a8aadeecf662f13794d6c243cef7bb08
yijiantao <yijiantao@hotmail.com>
2020-01-06 23:02:00 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/326.3-的幂.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=326 lang=python3
+#
+# [326] 3的幂
+#
+
+# @lc code=start
+import math
+class Solution:
+    # def isPowerOfThree(self, n: int) -> bool:
+    @classmethod
+    def isPowerOfThree(self, n: int) -> bool:
+        # round() 方法返回浮点数x的四舍五入值。
+        # math.log() 方法返回x的自然对数，x > 0
+        return n > 0 and 3 ** round(math.log(n, 3)) == n
+# @lc code=end
+
+print (Solution.isPowerOfThree(n = 27))
