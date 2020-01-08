fb7a24619bc0cfda454b721d820b4a4c708b3ee5
yijiantao <yijiantao@hotmail.com>
2020-01-08 22:49:49 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/172.阶乘后的零.py
+++ LeetCode Algorithms/172.阶乘后的零.py
@@ -10,10 +10,16 @@ class Solution:
     # def trailingZeroes(self, n: int) -> int:
     @classmethod
     def trailingZeroes(self, n):
-        res =  str(math.factorial(n))[::-1]
-        for _index, _value in enumerate(res):
-            if _value != '0': break
-        return res, _index
+        # 直接求阶乘，再统计的方法，当 n = 10000时 “Time Limit Exceeded”
+        # res =  str(math.factorial(n))[::-1]
+        # for _index, _value in enumerate(res):
+        #     if _value != '0': break
+        # return res, _index
+
+        # 方法二：
+        # 计算阶乘后面0的个数就是计算因数中有多少个5，所以计算5的个数即为5的个数
+        if n < 5: return 0
+        return int(n / 5 + Solution.trailingZeroes(n / 5))
 # @lc code=end
 
-print (Solution.trailingZeroes(n = 10000))
+print (Solution.trailingZeroes(n = 6))
