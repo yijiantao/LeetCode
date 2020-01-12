b704ecf87de9ca04ee4830aa5c1695b30e0bbaae
yijiantao <yijiantao@hotmail.com>
2020-01-12 11:48:35 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/100.相同的树.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=100 lang=python3
+#
+# [100] 相同的树
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        # 递归遍历树的节点
+        if not p and not q: return True
+        if not p or not q: return False
+        if p.val != q.val: return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+# @lc code=end
+
