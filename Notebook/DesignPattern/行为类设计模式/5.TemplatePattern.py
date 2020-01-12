42648d35595c1d8612f5e6a32bbd52c1471ebbfb
yijiantao <yijiantao@hotmail.com>
2020-01-12 12:05:26 +0800

    :notebook: 模板模式

--- Notebook/DesignPattern/行为类设计模式/5.TemplatePattern.py
+++ Notebook/DesignPattern/行为类设计模式/5.TemplatePattern.py
@@ -1,4 +1,4 @@
-# -*- coding -*- 
+# -*- coding -*-
 """
     一、定义：
         模板模式定义如下：定义一个操作中的算法的框架，而将一些步骤延迟到子类中，使得子类可以不改变一个算法的结构即可重新定义该算法的某些特定的步骤。
@@ -10,3 +10,70 @@
         根据股票代码来查询股价分为如下几个步骤：登录、设置股票代码、查询、展示。构造如下的虚拟股票查询器：
 """
 
+
+class StockQueryDevice():    # 虚拟股票查询器
+    stock_code = "0"
+    stock_price = 0.0
+
+    def login(self, usr, pwd):
+        pass
+
+    def setCode(self, code):
+        self.stock_code = code
+
+    def queryPrice(self):  # [模板模式]中，在父类中提供的模板方法
+        pass
+
+    def showPrice(self):  # [模板模式]中，在父类中提供的模板方法
+        pass
+
+    def operateQuery(self, usr, pwd, code):  # [模板模式]中，在父类中提供的模板方法
+        if not self.login(usr, pwd):
+            return False
+        self.setCode(code)
+        self.queryPrice()
+        self.showPrice()
+        return True
+
+
+class WebAStockQueryDevice(StockQueryDevice):
+    # [WebA查询器类] 根据不同的查询机构和查询方式，来通过继承的方式实现其对应的股票查询器类。
+    def login(self, usr, pwd):
+        if usr == "myStockA" and pwd == "myPwdA":
+            print("Web A:Login OK... user:%s pwd:%s" % (usr, pwd))
+            return True
+        else:
+            print("Web A:Login ERROR... user:%s pwd:%s" % (usr, pwd))
+            return False
+
+    def queryPrice(self):
+        print("Web A Querying...code:%s " % self.stock_code)
+        self.stock_price = 20.00
+
+    def showPrice(self):
+        print("Web A Stock Price...code:%s price:%s" %
+              (self.stock_code, self.stock_price))
+
+
+class WebBStockQueryDevice(StockQueryDevice):
+    # [WebB查询器类] 根据不同的查询机构和查询方式，来通过继承的方式实现其对应的股票查询器类。
+    def login(self, usr, pwd):
+        if usr == "myStockB" and pwd == "myPwdB":
+            print("Web B:Login OK... user:%s pwd:%s" % (usr, pwd))
+            return True
+        else:
+            print("Web B:Login ERROR... user:%s pwd:%s" % (usr, pwd))
+            return False
+
+    def queryPrice(self):
+        print("Web B Querying...code:%s " % self.stock_code)
+        self.stock_price = 30.00
+
+    def showPrice(self):
+        print("Web B Stock Price...code:%s price:%s" %
+              (self.stock_code, self.stock_price))
+
+
+if __name__ == "__main__":
+    web_a_query_dev = WebAStockQueryDevice()
+    web_a_query_dev.operateQuery("myStockA", "myPwdA", "12345")
