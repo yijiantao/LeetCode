a6dabe5777aee4071a8836cc18951b3d7348a3d2
yijiantao <yijiantao@hotmail.com>
2020-01-21 21:52:26 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/205.同构字符串.py
+++ LeetCode Algorithms/205.同构字符串.py
@@ -7,7 +7,7 @@
 # @lc code=start
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        
+        # 同构代表两个字符串中每个位置上字符在自身第一次出现的索引相同
         return [s.index(i) for i in s] == [t.index(i) for i in t]
 # @lc code=end
 
