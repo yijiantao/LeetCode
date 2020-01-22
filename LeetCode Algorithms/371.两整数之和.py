f9cd02de3cb9981db62f08ad6073e30c8d4357cd
yijiantao <yijiantao@hotmail.com>
2020-01-22 20:46:30 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/371.两整数之和.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=371 lang=python3
+#
+# [371] 两整数之和
+#
+
+# @lc code=start
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        # 方法一：
+        # return sum([a,b])
+
+        # 方法二：
+# @lc code=end
+
