234d1591ec0ffec0abd638e767a2d0393a78adcb
yijiantao <yijiantao@hotmail.com>
2020-01-22 20:53:03 +0800

    :bookmark: 异或运算+与运算

--- LeetCode Algorithms/371.两整数之和.py
+++ LeetCode Algorithms/371.两整数之和.py
@@ -25,7 +25,22 @@ class Solution:
             # 取余范围限制在 [0, 2^32-1] 范围内
             a = (a ^ b) % MASK
             b = carry % MASK
-        return a if a <= MAX_INT else ~((a % MIN_INT) ^ MAX_INT)   
+        return a if a <= MAX_INT else ~((a % MIN_INT) ^ MAX_INT)
+
+        """
+            注释：
+                那么问题就容易了，总结一下：
+
+                a + b 的问题拆分为 (a 和 b 的无进位结果) + (a 和 b 的进位结果)
+                无进位加法使用异或运算计算得出
+                进位结果使用与运算和移位运算计算得出
+                循环此过程，直到进位为 0
+                在 Python 中的特殊处理
+                在 Python 中，整数不是 32 位的，也就是说你一直循环左移并不会存在溢出的现象，这就需要我们手动对 Python 中的整数进行处理，手动模拟 32 位 INT 整型。
+
+                具体做法是将整数对 0x100000000 取模，保证该数从 32 位开始到最高位都是 0。
+
+        """
 
 # @lc code=end
 
