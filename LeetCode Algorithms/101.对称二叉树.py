4eec50bb648a9756e9e6df993ce3b8876b704195
yijiantao <yijiantao@hotmail.com>
2020-01-27 22:33:51 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/101.对称二叉树.py
+++ LeetCode Algorithms/101.对称二叉树.py
@@ -14,6 +14,22 @@
 
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
-        
+        # 递归实现
+        if not root: return True
+        def dfs(left, right):
+            # 递归的终止条件是两个节点都为空
+            # 或者两个节点中有一个为空
+            # 或者两个节点的值不相等
+            if not (left or right):
+                return True
+            if not (left and right):
+                return False
+            if left.val!=right.val:
+                return False
+            return dfs(left.left,right.right) and dfs(left.right,right.left)
+
+        # 用递归函数，比较左节点，右节点
+        return dfs(root.left,root.right)
+
 # @lc code=end
 
