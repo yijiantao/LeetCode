6be91bddbe57972b46f5b0f848061791be0461c9
yijiantao <yijiantao@hotmail.com>
2020-01-29 21:13:18 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/709.转换成小写字母.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=709 lang=python3
+#
+# [709] 转换成小写字母
+#
+
+# @lc code=start
+class Solution:
+    # def toLowerCase(self, str: str) -> str:
+    @classmethod
+    def toLowerCase(self, str):
+        return str.lower()
+# @lc code=end
+
+print(Solution.toLowerCase(str="Hello"))
