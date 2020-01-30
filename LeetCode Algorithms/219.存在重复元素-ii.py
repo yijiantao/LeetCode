bfda8aab06eaa990fb0d07f4072b21b84f73072c
yijiantao <yijiantao@hotmail.com>
2020-01-30 22:17:56 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/219.存在重复元素-ii.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=219 lang=python3
+#
+# [219] 存在重复元素 II
+#
+
+# @lc code=start
+class Solution:
+    # def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+    @classmethod
+    def containsNearbyDuplicate(self, nums, k):
+        # 运用字典 - 哈希表来解题
+        for _index, _value in enumerate(nums):
+# @lc code=end
+
