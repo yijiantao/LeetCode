af3d6f6ae9417931833389700e0486c2232e37cd
yijiantao <yijiantao@hotmail.com>
2020-02-03 16:37:30 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/40.组合总和-ii.py
+++ LeetCode Algorithms/40.组合总和-ii.py
@@ -9,9 +9,27 @@ class Solution:
     # def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
     @classmethod
     def combinationSum2(self, candidates, target):
+        res = []
+        if not candidates: return res
+
         candidates = sorted(candidates)
-        for _index in candidates:
-            print (candidates)
+        length = len(candidates)
+
+        # 回溯法标准模板
+        def backtrack_method(i, temp_sum, temp_list):
+            if temp_sum == target:
+                res.append(temp_list)
+                return
+
+            for _index in range(i, length):
+                if temp_sum + candidates[_index] > target: break    # 剪枝
+
+                if _index > i and candidates[_index] == candidates[_index - 1]: continue    # 剪枝
+                backtrack_method(_index + 1, temp_sum + candidates[_index], temp_list + [candidates[_index]])
+
+        backtrack_method(0, 0, [])
+
+        return res
         
 # @lc code=end
 
