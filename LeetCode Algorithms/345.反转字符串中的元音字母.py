6a1165f62f67146f5d7fd3a3cd5ec5a60db06ec4
yijiantao <yijiantao@hotmail.com>
2020-02-18 23:20:52 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/345.反转字符串中的元音字母.py
+++ LeetCode Algorithms/345.反转字符串中的元音字母.py
@@ -12,30 +12,15 @@ class Solution:
         s_list = list(s)
         left, right = 0, len(s_list) - 1
         while left < right:
-            if s_list[left] in o_list:
-                while left < right:
-                    if s_list[right] in o_list:
-                        s_list[left], s_list[right] = s_list[right], s_list[left]
-                        break
-                    else:
-                        right -= 1
-            else:
-                left +=
+            if s_list[left] not in o_list:
+                left += 1
+                continue
+            if s_list[right] not in o_list:
+                right -= 1
+                continue
+            s_list[left], s_list[right] = s_list[right], s_list[left]
+            left += 1; right -= 1
         return ''.join(s_list)
-
-        # o_list = ['a','e','i','o','u','A','E','I','O','U']
-        # s_list = list(s)
-        # left, right = 0, len(s_list) - 1
-        # while left < right:
-        #     if s_list[left] not in o_list:
-        #         left += 1
-        #         continue
-        #     if s_list[right] not in o_list:
-        #         right -= 1
-        #         continue
-        #     s_list[left], s_list[right] = s_list[right], s_list[left]
-        #     left += 1; right -= 1
-        # return ''.join(s_list)
 
 # @lc code=end
 
