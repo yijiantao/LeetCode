b1a9353be2b0bcad98e4a9f4c45d12aa869e9447
yijiantao <yijiantao@hotmail.com>
2020-03-05 15:51:38 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/1103.分糖果-ii.py
+++ LeetCode Algorithms/1103.分糖果-ii.py
@@ -8,20 +8,28 @@
 class Solution:
     @classmethod
     def distributeCandies(self, candies: int, num_people: int):
-        res = [0 for _ in range(num_people)]
-        _index, candies_index = -1, 0
-        while True:
-            candies_index += 1
-            _index += 1
-            _index %= num_people
-            if candies > candies_index:
-                res[_index] += candies_index
-                candies -= candies_index
-            else:
-                res[_index] += candies
-                break
+        # 简单方法一：
+        # res = [0 for _ in range(num_people)]
+        # _index, candies_index = -1, 0
+        # while True:
+        #     candies_index += 1
+        #     _index += 1
+        #     _index %= num_people
+        #     if candies > candies_index:
+        #         res[_index] += candies_index
+        #         candies -= candies_index
+        #     else:
+        #         res[_index] += candies
+        #         break
             
+        # 简单方法二：
+        res = [0] * num_people
+        _index = 0
+        while candies > 0:
+            res[_index % num_people] += min(_index + 1, candies)
+            candies -= (_index + 1)
+            _index += 1
         return res
 # @lc code=end
 
-print (Solution.distributeCandies(candies = 10, num_people = 3))
+print (Solution.distributeCandies(candies = 7, num_people = 4))
