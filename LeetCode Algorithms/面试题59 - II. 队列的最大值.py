ac785948d786a5bfe5eba9c750635dd264736144
yijiantao <yijiantao@hotmail.com>
2020-03-07 03:42:44 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/面试题59 - II. 队列的最大值.py
+++ LeetCode Algorithms/面试题59 - II. 队列的最大值.py
@@ -1,16 +1,16 @@
 class MaxQueue:
 
     def __init__(self):
-        
+        self.queue_list = []
 
     def max_value(self) -> int:
-        
+        return max(self.queue_list) if self.queue_list else -1
 
     def push_back(self, value: int) -> None:
-        
+        self.queue_list.append(value)
 
     def pop_front(self) -> int:
-        
+        return self.queue_list.pop(0) if self.queue_list else -1
 
 
 # Your MaxQueue object will be instantiated and called as such:
