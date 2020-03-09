ca53756be9ffb1e4f7dfdf999c5a6a7b7fed01f1
yijiantao <yijiantao@hotmail.com>
2020-03-09 11:52:38 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/121.best_time_to_buy_and_sell_stock.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+ 
+using namespace std;
+#include<vector>
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max_profit = 0;
+        if (prices.size() != 0){
+            int buy_price = prices[0];
+            for (int _day=1; _day < prices.size(); ++_day){
+                if (prices[_day] < buy_price){
+                    buy_price = prices[_day];
+                }
+                else{
+                    if ((prices[_day] - buy_price) > max_profit){
+                        max_profit = (prices[_day] - buy_price);
+                    }
+                }
+            }
+        }
+        return max_profit;
+    }
+};
+
+int main( )
+{
+    vector<int> prices = {7,6,4,3,1};
+    int minprice = 0, maxprofit = 0;
+    for (int price: prices) {
+        cout<<price<<endl;
+        maxprofit = max(maxprofit, price - minprice);
+        minprice = min(price, minprice);
+    }
+    // Solution s;
+    // int max_profit = s.maxProfit(prices);
+    // cout<<max_profit<<endl;
+    return 0;
+}
