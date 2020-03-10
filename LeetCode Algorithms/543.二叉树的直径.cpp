2d2a6df51b1d3f07c7c225e82d007c206f63bd98
yijiantao <yijiantao@hotmail.com>
2020-03-10 11:47:28 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/543.二叉树的直径.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=543 lang=cpp
+ *
+ * [543] 二叉树的直径
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    int res;
+    int dfs(TreeNode* node){
+        if (node == NULL) return 0;
+        int L = dfs(node->left);
+        int R = dfs(node->right);
+        res = max(res, L+R+1);
+        return max(L, R) + 1;
+    }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        res = 1;
+        dfs(root);
+        return res - 1;
+    }
+};
+// @lc code=end
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
