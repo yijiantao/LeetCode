81f40b9e1e38b8516bdf4503ea8edfd7d9a17b22
yijiantao <yijiantao@hotmail.com>
2020-03-13 15:39:13 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/169.多数元素.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=169 lang=cpp
+ *
+ * [169] 求众数
+ */
+
+// @lc code=start
+#include<bits/stdc++.h>
+// #include<map>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int nums_length = nums.size(), max_count = INT_MIN, res;
+        int half_nums_length = (int)nums_length / 2;
+        map<int, int> count_nums_dict;
+        for (int _value: nums){
+            if (count_nums_dict.count(_value) == 0) count_nums_dict[_value] = 0;
+            count_nums_dict[_value] += 1;
+        }
+        for (map<int, int>::iterator iter=count_nums_dict.begin(); iter != count_nums_dict.end(); iter++){
+            if (iter->second > max_count){
+                res = iter->first;
+                max_count = iter->second;
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
