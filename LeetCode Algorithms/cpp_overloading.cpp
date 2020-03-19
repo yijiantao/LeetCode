8825500d7ac81e730160a21bc956bddaa00941b0
yijiantao <yijiantao@hotmail.com>
2020-03-19 15:38:11 +0800

    :tada: test_code for cpp

---
+++ LeetCode Algorithms/cpp_overloading.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+    https://www.bilibili.com/video/av9468549?p=90
+    P90: 函数重载练习，实现不同数据类型数组的排序。
+    int iNums[] = {56, 54, 12, 89, 43};
+    float fNums[] = {78.0f, 5.7f, 42.8f, 99.1f};
+    double dNums[] = {78.9, 23.6, 77.8, 98.5, 33.3};
+*/
+void Sort(int[]);
+void Sort(float[]);
+void Sort(double[]);
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
