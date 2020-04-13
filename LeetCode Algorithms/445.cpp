6511dee4da9c9a1539777a6cfc36b3bb42b2a94c
yijiantao <yijiantao@hotmail.com>
2020-04-14 00:09:19 +0800

    :bookmark: to do list

---
+++ LeetCode Algorithms/445.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+
+445. 两数相加 II
+给你两个 [非空] 链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储一位数字。将这两数相加会返回一个新的链表。
+
+你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+
+ 
+进阶：
+
+如果输入链表不能修改该如何处理？换句话说，你不能对列表中的节点进行翻转。
+
+
+示例：
+
+输入：(7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 8 -> 0 -> 7
+
+通过次数18,385提交次数33,412
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution s;
+    s.addTwoNumbers();
+
+    return 0;
+}
