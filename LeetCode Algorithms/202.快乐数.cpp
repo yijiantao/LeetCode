9004e6aba267e87676261b5065bb0a252726608b
yijiantao <yijiantao@hotmail.com>
2020-04-30 21:48:02 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/202.快乐数.cpp
+++ LeetCode Algorithms/202.快乐数.cpp
@@ -2,36 +2,30 @@
  * @lc app=leetcode.cn id=202 lang=cpp
  *
  * [202] 快乐数
- * 编写一个算法来判断一个数 n 是不是快乐数。
-
-「快乐数」定义为：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。如果 可以变为  1，那么这个数就是快乐数。
-
-如果 n 是快乐数就返回 True ；不是，则返回 False 。
-
- 
-
-示例：
-
-输入：19
-输出：true
-解释：
-12 + 92 = 82
-82 + 22 = 68
-62 + 82 = 100
-12 + 02 + 02 = 1
-
-来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/happy-number
-著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 
  */
 
 // @lc code=start
 class Solution {
 public:
+    int squareSum(int m) {
+        int sum = 0;
+        while (m > 0) {
+            int temp_num = m % 10;
+            sum += (temp_num * temp_num);
+            m /= 10;
+        }
+        return sum;
+    }
+
     bool isHappy(int n) {
 
-        while 
-        return 
+        set<int> seen = {};
+        while (n != 1 && seen.find(n) == seen.end()) {   // set find函数 没查找到 即 == set.end()
+            seen.insert(n);
+            n = squareSum(n);
+        }
+        return n == 1;
     }
 };
 // @lc code=end
