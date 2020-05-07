6d4d5033e4be6583c0b5037b5091667c7dc8dda6
yijiantao <yijiantao@hotmail.com>
2020-05-07 08:12:56 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/572.另一个树的子树.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=572 lang=cpp
+ *
+ * [572] 另一个树的子树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubtree(TreeNode* s, TreeNode* t) {
+
+    }
+};
+// @lc code=end
+
