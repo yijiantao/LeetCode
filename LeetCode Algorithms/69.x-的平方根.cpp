22c41c2412c44acd6f39f1dfab7f1558b3831173
yijiantao <yijiantao@hotmail.com>
2020-05-09 21:46:51 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/69.x-的平方根.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=69 lang=cpp
+ *
+ * [69] x 的平方根
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0) return 0;
+        int ans = exp(0.5 * log(x));
+        return ((long long)(ans + 1) * (ans + 1) <= x ? ans + 1 : ans);
+    }
+};
+// @lc code=end
+
