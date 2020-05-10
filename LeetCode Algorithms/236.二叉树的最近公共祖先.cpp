e68978a6fd8c2fd9bedd8784c9a4df48156244ed
yijiantao <yijiantao@hotmail.com>
2020-05-10 16:06:13 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/236.二叉树的最近公共祖先.cpp
+++ LeetCode Algorithms/236.二叉树的最近公共祖先.cpp
@@ -17,7 +17,17 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
+        if (root == NULL) return NULL;
+        if (root == p || root == q) return root;
+
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if (left == NULL) return right;
+        if (right == NULL) return left;
+
+        if (left && right) return root;
+        return NULL;
     }
 };
 // @lc code=end
