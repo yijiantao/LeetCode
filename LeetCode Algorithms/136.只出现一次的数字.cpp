9602a54838e57e186e24ec186305024d3236afa2
yijiantao <yijiantao@hotmail.com>
2020-05-14 14:11:57 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/136.只出现一次的数字.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=cpp
+ *
+ * [136] 只出现一次的数字
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for (auto _v: nums) ans ^= _v;
+        return ans;
+    }
+};
+// @lc code=end
+
