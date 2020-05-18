d9823ebad5adc36070c436f7df69d47ae561c36e
yijiantao <yijiantao@hotmail.com>
2020-05-18 14:54:30 +0800

    :bookmark: 《穷查理宝典》读书笔记

---
+++ LeetCode Algorithms/152.乘积最大子数组.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=152 lang=cpp
+ *
+ * [152] 乘积最大子数组
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int maxF = nums[0], minF = nums[0], ans = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            int mx = maxF, mn = minF;
+            maxF = max(mx * nums[i], max(nums[i], mn * nums[i]));
+            minF = min(mn * nums[i], min(nums[i], mx * nums[i]));
+            ans = max(maxF, ans);
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
