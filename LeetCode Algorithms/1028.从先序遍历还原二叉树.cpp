35728e50864078e6c6318cdff2453a8bc19cf57b
yijiantao <yijiantao@hotmail.com>
2020-06-18 22:59:59 +0800

    [wait AC]

---
+++ LeetCode Algorithms/1028.从先序遍历还原二叉树.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1028 lang=cpp
+ *
+ * [1028] 从先序遍历还原二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* recoverFromPreorder(string S) {
+
+    }
+};
+// @lc code=end
+
