165fa41e8f4fa9b3488bc92a65ee8c053b398b11
yijiantao <yijiantao@hotmail.com>
2020-06-21 22:04:21 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/124.二叉树中的最大路径和.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=124 lang=cpp
+ *
+ * [124] 二叉树中的最大路径和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        if (root == nullptr) return 0;
+        int val = INT_MIN;
+        dfs(root, val);
+        return val;
+    }
+    
+    // 返回经过root的单边分支最大和， 即Math.max(root, root+left, root+right)
+    int dfs(TreeNode* root, int &val) {
+        if (root == nullptr) return 0;
+        //计算左边分支最大值，左边分支如果为负数还不如不选择
+        int left = std::max(dfs(root->left, val), 0);
+
+        int right = std::max(dfs(root->right, val), 0);
+
+        int tmp_val = root->val + left + right;
+        val = std::max(val, tmp_val);
+
+        return root->val + std::max(left, right);
+    }
+};
+// @lc code=end
+
