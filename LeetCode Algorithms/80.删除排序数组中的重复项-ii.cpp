31213299e8cc730b27cbe53fc3d2aab5bc6c2ab1
yijiantao <yijiantao@hotmail.com>
2020-07-18 20:55:40 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/80.删除排序数组中的重复项-ii.cpp
+++ LeetCode Algorithms/80.删除排序数组中的重复项-ii.cpp
@@ -7,14 +7,16 @@
 // @lc code=start
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
+    int removeDuplicates(vector<int>& nums) { // [0,0,1,1,1,1,2,3,3]
         int nums_length = nums.size();
-        if (nums_length <= 3) return nums_length;
-        int slow_p_index = 0, fast_p_index = 2;
-        for (int _index = 1; _index < nums_length; ++_index) {
-            
+        if (nums_length < 3) return nums_length;
+        int slow_p_index = 1;
+        for (int fast_p_index = 2; fast_p_index < nums_length; ++fast_p_index) {
+            if (nums[fast_p_index] != nums[slow_p_index - 1]) {
+                nums[++slow_p_index] = nums[fast_p_index];    
+            }
         }
-        return ;
+        return slow_p_index + 1;
     }
 };
 // @lc code=end
