f13e0619aa3b0912fc30dfd1ba8172cd45aa7b08
yijiantao <yijiantao@hotmail.com>
2020-07-20 22:43:01 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/167.两数之和-ii-输入有序数组.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=167 lang=cpp
+ *
+ * [167] 两数之和 II - 输入有序数组
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left=0, right=numbers.size()-1;
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target)
+                return {left + 1, right + 1};
+            else if (numbers[left] + numbers[right] < target)
+                ++left;
+            else --right;
+        }
+        return {-1, -1};
+    }
+};
+// @lc code=end
+
