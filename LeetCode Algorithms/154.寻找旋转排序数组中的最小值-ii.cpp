9eb890cbb753a6a3e84dba56c214286274c2b316
yijiantao <yijiantao@hotmail.com>
2020-07-22 22:15:26 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/154.寻找旋转排序数组中的最小值-ii.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=154 lang=cpp
+ *
+ * [154] 寻找旋转排序数组中的最小值 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int res = nums[0];
+        for (int _index = 1; _index < nums.size(); ++_index) 
+            if (nums[_index-1] > nums[_index]) return nums[_index];
+        return res;
+    }
+};
+// @lc code=end
+
