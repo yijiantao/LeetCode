35a806704d19e6f099abaef9024d33fc20ce6701
yijiantao <yijiantao@hotmail.com>
2020-07-29 23:01:17 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/299.猜数字游戏.cpp
+++ LeetCode Algorithms/299.猜数字游戏.cpp
@@ -9,7 +9,18 @@ class Solution {
 public:
     string getHint(string secret, string guess) {
         string res;
-
+        int a = 0, b =0;
+        int hash[10]={0};
+        int n=secret.size();
+        for (int i=0; i< n; ++i) {
+            hash[secret[i]-'0']++;
+            hash[guess[i]-'0']--;
+            if (secret[i] == guess[i]) a++;
+        }
+        int df=0;
+        for (int i =0;i<10;i++) if (hash[i] >0) df +=hash[i];
+        b=n-a-df;
+        res=to_string(a)+"A"+to_string(b)+"B";
         return res;
     }
 };
