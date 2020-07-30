79d1b6058bacaed834a53455cb2c6202d88cf6f0
yijiantao <yijiantao@hotmail.com>
2020-07-30 22:45:10 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/343.整数拆分.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=343 lang=cpp
+ *
+ * [343] 整数拆分
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int integerBreak(int n) {
+        int res;
+
+        return res;
+    }
+};
+// @lc code=end
+
