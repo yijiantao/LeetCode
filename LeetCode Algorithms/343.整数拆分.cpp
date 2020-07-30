3dcafd7d104cf1a4fc528776f1cede57def41407
yijiantao <yijiantao@hotmail.com>
2020-07-30 23:21:16 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/343.整数拆分.cpp
+++ LeetCode Algorithms/343.整数拆分.cpp
@@ -9,8 +9,11 @@ class Solution {
 public:
     int integerBreak(int n) {
         int res;
-
-        return res;
+        if (n <= 3) return n - 1;
+        int a = n / 3, b = n % 3;
+        if (b == 0) return (int)(pow(3, a));
+        if (b == 1) return (int)(pow(3, a - 1) * 4);
+        return (int)(pow(3, a) * 2);
     }
 };
 // @lc code=end
