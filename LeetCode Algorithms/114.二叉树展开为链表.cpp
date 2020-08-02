73be6df36f962a9bd88f0ee680af2ad64de3ac4b
yijiantao <yijiantao@hotmail.com>
2020-08-02 22:05:04 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/114.二叉树展开为链表.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=114 lang=cpp
+ *
+ * [114] 二叉树展开为链表
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        TreeNode* tmp_root = root;
+        while (tmp_root->val)
+        {
+            if (tmp_root->left) {
+                tmp_root;
+            }
+        }
+        
+    }
+};
+// @lc code=end
+
