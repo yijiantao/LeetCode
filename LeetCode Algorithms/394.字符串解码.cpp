85ae222bbf782d180912a7c756b9c2bb702c400a
yijiantao <yijiantao@hotmail.com>
2020-08-20 23:17:36 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/394.字符串解码.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=394 lang=cpp
+ *
+ * [394] 字符串解码
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string decodeString(string s) {
+        string res_s;
+        stack<char> res;
+        stack<int> nums;
+        for (auto _c: s) {
+            
+        }
+
+    }
+};
+// @lc code=end
+
