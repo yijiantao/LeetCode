6ab2fe11ec9c84a87aa0d478a7053ec902ae6f12
yijiantao <yijiantao@hotmail.com>
2020-08-22 23:11:15 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/860.柠檬水找零.cpp
+++ LeetCode Algorithms/860.柠檬水找零.cpp
@@ -3,7 +3,8 @@
  *
  * [860] 柠檬水找零
  */
-
+#include<bits/stdc++.h>
+using namespace std;
 // @lc code=start
 class Solution {
 public:
@@ -14,11 +15,20 @@ public:
                 nums_5 += 1;
                 continue;
             } else if (_v == 10) {
-                if (nums_5 == 0) return false;
+                if (nums_5 <= 0) return false;
                 nums_5 -= 1;
                 nums_10 += 1;
             } else if (_v == 20) {
-                if (nums_5 == 0) return false;
+                cout << nums_5 << ' ' << nums_10 << endl;
+                if (nums_5 <= 0) return false;
+                if (nums_5 * 5 + nums_10 * 10 < 20) return false;
+                if (nums_10 >= 1 && nums_5 >= 1) {
+                    nums_5 -= 1;
+                    nums_10 -= 1;
+                }
+                if (nums_5 >= 3) {
+                    nums_5 -= 3;
+                }
             }
         }
         return true;
@@ -26,3 +36,11 @@ public:
 };
 // @lc code=end
 
+
+int main(int argc, char const *argv[])
+{
+    vector<int> bills = {5,5,5,10,5,5,10,20,20,20};
+    Solution s;
+    cout << s.lemonadeChange(bills) << endl;
+    return 0;
+}
