82085c907199ac2cd8a0241570adb71dd2fec681
yijiantao <yijiantao@hotmail.com>
2020-08-25 22:54:37 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/1185.一周中的第几天.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=1185 lang=cpp
+ *
+ * [1185] 一周中的第几天
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string dayOfTheWeek(int day, int month, int year) {
+        if (month==1 || month==2) month+=12, year--;
+        int iWeek = (day+2*month+3*(month+1)/5+year+year/4-year/100+year/400)%7;
+        string res[]={"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        return res[iWeek];
+    }
+};
+// @lc code=end
+
