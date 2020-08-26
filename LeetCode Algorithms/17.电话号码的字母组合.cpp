62ef076ec3f7f56ef5ef7a75da17c3b44f096da9
yijiantao <yijiantao@hotmail.com>
2020-08-26 23:41:38 +0800

    [redo][DFS][回溯法]更新刷题代码+说明文档

---
+++ LeetCode Algorithms/17.电话号码的字母组合.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=17 lang=cpp
+ *
+ * [17] 电话号码的字母组合
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        map<char, vector<char>> digit2char  {
+            {'2': {'a', 'b', 'c'}},
+            {'3': {'d', 'e', 'f'}},
+            {'4': {'g', 'h', 'i'}},
+            {'5': {'j', 'k', 'l'}},
+            {'6': {'m', 'n', 'o'}},
+            {'7': {'p', 'q', 'r', 's'}},
+            {'8': {'t', 'u', 'v'}},
+            {'9': {'w', 'x', 'y', 'z'}},
+        }
+
+    }
+};
+// @lc code=end
+
