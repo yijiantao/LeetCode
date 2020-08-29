a0b48b111cdd92f2540172d83081e81d7b197753
yijiantao <yijiantao@hotmail.com>
2020-08-29 20:30:46 +0800

    [redo] DP -  马拉车KMP算法

---
+++ LeetCode Algorithms/214.最短回文串.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=214 lang=cpp
+ *
+ * [214] 最短回文串
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        // 马拉车算法 DP
+        return 
+    }
+};
+// @lc code=end
+
