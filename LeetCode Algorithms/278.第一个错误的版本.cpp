8696752d2ae37a2edfb1da3d154b02dc16ffd04e
yijiantao <yijiantao@hotmail.com>
2020-09-07 21:27:26 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/278.第一个错误的版本.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=278 lang=cpp
+ *
+ * [278] 第一个错误的版本
+ */
+
+// @lc code=start
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
+// @lc code=end
+
