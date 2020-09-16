6e73046b4860bcd7e66bb1530df948ca70545079
yijiantao <yijiantao@hotmail.com>
2020-09-16 21:49:21 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/728.自除数.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=728 lang=cpp
+ *
+ * [728] 自除数
+ */
+
+// @lc code=start
+class Solution {
+public:
+
+    bool isDiv(int nums) {
+        while (nums)
+        {
+            if ((nums % (nums % 10)) != 0) return false;
+            nums /= 10;
+        }
+        return true;
+    }
+
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> res;
+        for (int _v = left; _v <= right; ++_v) {
+            if (isDiv(_v)) res.push_back(_v);
+        }
+        return res;
+    }
+};
+// @lc code=end
+
