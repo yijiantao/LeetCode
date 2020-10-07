97e66a5b81b53f61b33bb450d3d583f49f29d4d2
yijiantao <yijiantao@hotmail.com>
2020-10-07 22:42:08 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/75.颜色分类.cpp
+++ LeetCode Algorithms/75.颜色分类.cpp
@@ -9,20 +9,19 @@
 class Solution {
 public:
     void sortColors(std::vector<int>& nums) {
-        // int left_index = 0, right_index = nums.size() - 1;
-        // for (int _index = 0; _index <= right_index; ++_index) {
-        //     if (nums[_index] == 0) std::swap(nums[left_index++], nums[_index]);
-        //     if (nums[_index] == 2) std::swap(nums[right_index--], nums[_index--]);
-        // }
-        std::sort(nums.begin(), nums.end());
+        int left_index = 0, right_index = nums.size() - 1;
+        for (int _index = 0; _index <= right_index; ++_index) {
+            if (nums[_index] == 0) std::swap(nums[left_index++], nums[_index]);
+            if (nums[_index] == 2) std::swap(nums[right_index--], nums[_index--]);
+        }
     }
 };
 // @lc code=end
 
-// int main(int argc, char const *argv[])
-// {
-//     Solution s;
-//     std::vector<int> nums = {2,0,1};
-//     s.sortColors(nums);
-//     return 0;
-// }
+int main(int argc, char const *argv[])
+{
+    Solution s;
+    std::vector<int> nums = {2,0,1};
+    s.sortColors(nums);
+    return 0;
+}
