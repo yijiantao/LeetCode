931c313a588789a698077b0a8c027e0ae6f9f726
yijiantao <yijiantao@hotmail.com>
2020-11-16 23:45:37 +0800

    [add] c for unix network programming

---
+++ Notebook/cpp_notebook/project/Web/unixNetWorkProgram/udpServer.c
@@ -0,0 +1,14 @@
+#include "lib/common.h"
+
+static int count;
+
+static void recvfrom_int(int signo) {
+    printf("\nrecvived %d datagrams\n", count);
+    exit(0);
+}
+
+int main(int argc, char **argv) {
+    int socket_fd;
+    
+    return 0;
+}
