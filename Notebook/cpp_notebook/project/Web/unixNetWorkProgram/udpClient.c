e34468bbe155895259ab80be6684d69a8056d2c2
yijiantao <yijiantao@hotmail.com>
2020-11-20 23:08:57 +0800

    [add] c for unix network programming

--- Notebook/cpp_notebook/project/Web/unixNetWorkProgram/udpClient.c
+++ Notebook/cpp_notebook/project/Web/unixNetWorkProgram/udpClient.c
@@ -4,3 +4,10 @@
 # define    DGLEN       1400    /* length of each datagram */
 # define    MAXLINE     4096
 
+int main(int argc, char **argv) {
+
+    if (argc ! = 2) {
+        error(1, 0, )
+    }
+    return 0;
+}
