15453709e979a9d126aeb88a5404c0e985996950
yijiantao <yijiantao@hotmail.com>
2020-11-22 22:39:17 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/242.有效的字母异位词.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=242 lang=cpp
+ *
+ * [242] 有效的字母异位词
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        std::sort(s.begin(), s.end());
+        std::sort(t.begin(), t.end());
+        cout <<t <<endl;
+        return s == t;
+    }
+};
+// @lc code=end
+
