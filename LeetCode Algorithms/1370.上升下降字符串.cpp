9d0e72560093f7014169bd0266bce072135c0497
yijiantao <yijiantao@hotmail.com>
2020-11-25 22:21:44 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/1370.上升下降字符串.cpp
+++ LeetCode Algorithms/1370.上升下降字符串.cpp
@@ -8,7 +8,17 @@
 class Solution {
 public:
     string sortString(string s) {
-        
+        vector<short> v(26);
+        for (auto c:s) v[c - 'a']++;
+
+        string ans(s);
+        int index = 0;
+        while (s.size() > index)
+        {
+            for (int i = 0; i < 26; ++i) if (v[i] > 0 && v[i]--) ans[index++] = 'a' + i;
+            for (int i = 25; ~i; --i) if (v[i] > 0 && v[i]--) ans[index++] = 'a' + i;
+        }
+        return ans;
     }
 };
 // @lc code=end
