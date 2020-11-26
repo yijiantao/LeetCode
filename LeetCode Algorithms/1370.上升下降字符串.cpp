c78e4c445ca72b621924e48a66a7ee0acf6df2f4
yijiantao <yijiantao@hotmail.com>
2020-11-26 23:43:17 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/1370.上升下降字符串.cpp
+++ LeetCode Algorithms/1370.上升下降字符串.cpp
@@ -13,6 +13,7 @@ public:
 
         string ans(s);
         int index = 0;
+        
         while (s.size() > index)
         {
             for (int i = 0; i < 26; ++i) if (v[i] > 0 && v[i]--) ans[index++] = 'a' + i;
