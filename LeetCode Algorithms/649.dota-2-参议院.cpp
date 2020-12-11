f49217c45a82fc7b0e720333a2f9b518e983db48
yijiantao <yijiantao@hotmail.com>
2020-12-11 21:39:07 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/649.dota-2-参议院.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=649 lang=cpp
+ *
+ * [649] Dota2 参议院
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+
+    }
+};
+// @lc code=end
+
