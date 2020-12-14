8344b419330aa1342f4cbb16db517354dc5122c1
yijiantao <yijiantao@hotmail.com>
2020-12-14 23:55:54 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/49.字母异位词分组.cpp
+++ LeetCode Algorithms/49.字母异位词分组.cpp
@@ -8,7 +8,21 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-
+        vector<vector<string>> res;
+        unordered_map<string, int> hash_map;
+        int tag = 1;
+        string temp;
+        for (string& str: strs) {
+            temp = str;
+            sort(str.begin(), str.end());
+            if (hash_map[str] == 0) {
+                hash_map[str] = tag++;
+                res.push_back({temp});
+            } else {
+                res[hash_map[str] - 1].push_back(temp);
+            }
+        }
+        return res;
     }
 };
 // @lc code=end
