d8221aaf021835d29a69e2be2691d57e6b5570e3
yijiantao <yijiantao@hotmail.com>
2020-12-18 22:08:53 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/389.找不同.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=389 lang=cpp
+ *
+ * [389] 找不同
+ */
+
+// @lc code=start
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        for (int _index = 0; _index < s.size(); ++_index) {
+            if (s[_index] != t[_index]) {
+                return t[_index];
+            }
+        }
+        
+        return t[t.size()-1];
+    }
+};
+// @lc code=end
+
