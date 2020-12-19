72b7a2afb8e0e2be6abab99a2c2ca7151c509738
yijiantao <yijiantao@hotmail.com>
2020-12-19 20:38:55 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/48.旋转图像.cpp
+++ LeetCode Algorithms/48.旋转图像.cpp
@@ -11,10 +11,13 @@ public:
         int row_len = matrix.size(), col_len = matrix[0].size();
         // 矩阵转置
         for (int row_index = 0; row_index < row_len; ++row_index)
-            for (int col_index = )
-            
+            for (int col_index = row_index+1; col_index < col_len; ++col_index)
+                swap(matrix[row_index][col_index], matrix[col_index][row_index]);
+
         // 矩阵沿中轴线翻转
-        return matrix;
+        for (int row_index = 0; row_index < row_len; ++row_index)
+            for (int col_index = 0; col_index < col_len / 2; ++col_index)
+                swap(matrix[row_index][col_index], matrix[row_index][col_len-col_index-1]);
     }
 };
 // @lc code=end
