d0cdc100f97e524ecfbe6ede10fc43c932ca7ac4
yijiantao <yijiantao@hotmail.com>
2020-12-25 23:04:33 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/455.分发饼干.cpp
+++ LeetCode Algorithms/455.分发饼干.cpp
@@ -10,8 +10,13 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
-        
-        return 0;
+        int child_nums = g.size(), cook_nums = s.size();
+        int count =0;
+        for (int i = 0, j = 0; i < child_nums && j < cook_nums; i++, j++) {
+            while (j < child_nums && g[i] > s[j]) j++;
+            if (j < child_nums) count ++;
+        }
+        return count;
     }
 };
 // @lc code=end
