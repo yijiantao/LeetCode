3974e358cb78b82704cd5eebc0373f6e6cb35c46
yijiantao <yijiantao@hotmail.com>
2020-12-25 23:01:42 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/455.分发饼干.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=455 lang=cpp
+ *
+ * [455] 分发饼干
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        
+        return 0;
+    }
+};
+// @lc code=end
+
