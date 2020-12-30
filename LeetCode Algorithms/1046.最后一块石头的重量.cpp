3bbfcf88804739658e6c095e88155e94c077d6f6
yijiantao <yijiantao@hotmail.com>
2020-12-30 22:54:00 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/1046.最后一块石头的重量.cpp
+++ LeetCode Algorithms/1046.最后一块石头的重量.cpp
@@ -8,7 +8,18 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        
+        priority_queue<int> q;
+        for (int v: stones) q.push(v);
+
+        while (q.size() > 1) {
+            int a = q.top();
+            q.pop();
+            int b = q.top();
+            q.pop();
+
+            if (a > b) q.push(a-b);
+        }
+        return q.empty() ? 0 : q.top();
     }
 };
 // @lc code=end
