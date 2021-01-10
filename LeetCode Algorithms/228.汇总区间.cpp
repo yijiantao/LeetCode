a31e24ca78d6a415dfde44bf4d81ea3cc945a05f
yijiantao <yijiantao@hotmail.com>
2021-01-10 20:50:01 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/228.汇总区间.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=228 lang=cpp
+ *
+ * [228] 汇总区间
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> res;
+        int i = 0, n = nums.size();
+        while (i < n) {
+            int low = i;
+            i++;
+            while ()
+        }
+    }
+};
+// @lc code=end
+
