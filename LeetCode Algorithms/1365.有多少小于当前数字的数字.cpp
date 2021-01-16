e0d6895fc1cb31440e0797583b639cedd0c42298
yijiantao <yijiantao@hotmail.com>
2021-01-16 21:24:12 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/1365.有多少小于当前数字的数字.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1365 lang=cpp
+ *
+ * [1365] 有多少小于当前数字的数字
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+
+    }
+};
+// @lc code=end
+
