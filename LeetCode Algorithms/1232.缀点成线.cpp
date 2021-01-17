6ba3253ec5c18795ec54ed64bb64d928ac9281fa
yijiantao <yijiantao@hotmail.com>
2021-01-17 20:31:08 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/1232.缀点成线.cpp
+++ LeetCode Algorithms/1232.缀点成线.cpp
@@ -8,8 +8,14 @@
 class Solution {
 public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
-
-        
+        if (coordinates.size() < 3) return true;
+        int k = (coordinates[1][1] - coordinates[1][0]) / (coordinates[0][1] - coordinates[0][0]);;
+        for (int _index = 2; _index < coordinates.size(); ++_index) {
+            int tmp_k = (coordinates[_index][1] - coordinates[_index][0]) / (coordinates[_index-1][1] - coordinates[_index-1][0]);
+            if (k == tmp_k) k = tmp_k;
+            else return false;
+        }
+        return true;
     }
 };
 // @lc code=end
