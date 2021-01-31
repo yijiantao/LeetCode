4d99d4be38b2f0da913c27098b4595d9e795d81d
yijiantao <yijiantao@hotmail.com>
2021-01-31 21:29:30 +0800

    :tada: 更新刷题代码+说明文档

--- LeetCode Algorithms/1672.最富有客户的资产总量.cpp
+++ LeetCode Algorithms/1672.最富有客户的资产总量.cpp
@@ -8,8 +8,13 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        int ans = 0;
-        
+        int ans = 0, tmp = 0;
+        for (int r=0; r<accounts.size(); ++r) {
+            tmp =0;
+            for (int c=0; c<accounts[r].size(); ++c)
+                tmp += accounts[r][c];
+            if (ans < tmp) ans = tmp;
+        }
         return ans;
     }
 };
