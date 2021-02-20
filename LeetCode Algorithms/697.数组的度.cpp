6a0dea8250b2dfe02d419086ee6ead8f2224fbe7
yijiantao <yijiantao@hotmail.com>
2021-02-20 22:19:29 +0800

    :tada: 更新刷题代码+说明文档

---
+++ LeetCode Algorithms/697.数组的度.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=697 lang=cpp
+ *
+ * [697] 数组的度
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findShortestSubArray(vector<int>& nums) {
+
+    }
+};
+// @lc code=end
+
