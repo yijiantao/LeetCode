e2e7e6da9243a6d7fff56a0f8aa4349f512fe3be
yijiantao <yijiantao@hotmail.com>
2021-11-20 23:12:46 +0800

    [add] Q859

---
+++ LeetCode Algorithms/859.亲密字符串.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=859 lang=cpp
+ *
+ * [859] 亲密字符串
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        return false;
+    }
+};
+// @lc code=end
+
