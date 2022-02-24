775afc0e79e847a2201413283d6e15dd8282869d
yijiantao <yijiantao@hotmail.com>
2022-02-24 23:25:48 +0800

    [add] linklist op

---
+++ LeetCode Algorithms/203.移除链表元素.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=cpp
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* p, q;
+        
+        while (head){
+            cout << p.val;
+            p = p->next;
+        }
+            
+        return head;
+    }
+};
+// @lc code=end
+
