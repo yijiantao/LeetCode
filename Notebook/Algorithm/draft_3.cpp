9e92aee18d4452917af6a6e5b7edbb37e17a025f
yijiantao <yijiantao@hotmail.com>
2022-03-28 22:59:21 +0800

    [modified] static_cast类型强转

--- Notebook/Algorithm/draft_3.cpp
+++ Notebook/Algorithm/draft_3.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
 #include <vector>
 #include <stdexcept>
+#include <typeinfo>
 
-void print_size(size_t idx, std::vector<int> a)
+void print_size(size_t idx, std::vector<int> arr)
 {
     std::vector<int> a (10);
     std::cout << idx << std::endl;
 }
 
-double print_size(const )
-{
-    double res = 0.0;
-    for (auto _s:)
-    return double;
-}
+// double print_size(const )
+// {
+//     double res = 0.0;
+//     for (auto _s:)
+//     return double;
+// }
 
-decltype(arr) print_size()
-{
+// decltype(arr) print_size()
+// {
 
-}
+// }
 
-std::string::size_type print_size(const std::string &s)
-{
+// std::string::size_type print_size(const std::string &s)
+// {
 
-}
+// }
 
 inline const std::string &
 find_short_string(const std::string &s1, const std::string &s2)
@@ -36,18 +37,23 @@ int main(int argc, char const *argv[])
     // std::string
     std::string name = "yijiantao";
     for (auto &c: name){
-        c = 'x';
+        if (c == 'a') c = 'x';
     }
     std::cout << name << std::endl;
 
     // std::vector
     std::vector<int> num(10, 42);
     for (auto _v: num) std::cout << _v << std::endl;
-    auto mid = num.begin() + num.size() / 2;
+    // auto mid = num.begin() + num.size() / 2;
+    auto mid = num.size() / 2;
     std::cout << mid << std::endl;
 
     // 类型转换
-    double double_name = static_cast<double> name;
+    // std::string str_num (10, '1');   // string static_cast强转出错
+    int str_num = 1;
+    double double_name = static_cast<double> (str_num);
+    std::cout << str_num << ' ' << typeid(str_num).name() << std::endl;
+    std::cout << double_name << ' ' << typeid(double_name).name() << std::endl;
 
     // 异常捕获
     // try{
@@ -65,6 +71,6 @@ int main(int argc, char const *argv[])
 
     // }
 
-    std::cout << find_short_string() << std::endl;
+    std::cout << find_short_string(name, name+ " changed") << std::endl;
     return 0;
 }
