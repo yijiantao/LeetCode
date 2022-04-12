4d92dc2a7b4e63cb39f65c7bb8581b8ea1bbad9b
yijiantao <yijiantao@hotmail.com>
2022-04-12 23:05:18 +0800

    [update] format style

--- Notebook/Algorithm/test/change.cpp
+++ Notebook/Algorithm/test/change.cpp
@@ -1,23 +1,20 @@
-#include <iostream>
-#include <memory>
 #include <fstream>
-#include <sstream>
-#include <vector>
-#include <string>
+#include <iostream>
 #include <map>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <vector>
 
-bool read_file(std::string file_name, std::vector<std::string> &file_value)
-{
+bool read_file(std::string file_name, std::vector<std::string> &file_value) {
     std::string data_buffer;
     std::ifstream infile_handler(file_name);
-    if (!infile_handler.is_open())
-    {
+    if (!infile_handler.is_open()) {
         std::cout << "Erro opening file!" << std::endl;
         return false;
     }
 
-    while (!infile_handler.eof())
-    {
+    while (!infile_handler.eof()) {
         getline(infile_handler, data_buffer);
         file_value.push_back(data_buffer);
     }
@@ -25,28 +22,24 @@ bool read_file(std::string file_name, std::vector<std::string> &file_value)
     return true;
 }
 
-bool write_file(std::string file_name, std::vector<std::string> change_strings)
-{
+bool write_file(std::string file_name, std::vector<std::string> change_strings) {
     std::ofstream outfile_handler(file_name);
     if (!outfile_handler.is_open()) {
         return false;
     }
-    for (auto _s: change_strings) {
+    for (auto _s : change_strings) {
         outfile_handler << _s;
     }
     outfile_handler.close();
     return true;
 }
 
-std::map<std::string, std::string> get_rule(std::string file_name)
-{
+std::map<std::string, std::string> get_rule(std::string file_name) {
     std::string buff;
     std::map<std::string, std::string> rule;
     std::ifstream infile_handle(file_name);
-    if (infile_handle.is_open())
-    {
-        while (!infile_handle.eof())
-        {
+    if (infile_handle.is_open()) {
+        while (!infile_handle.eof()) {
             getline(infile_handle, buff);
             size_t pos = buff.find(' ');
             rule[buff.substr(0, pos)] = buff.substr(pos + 1, buff.length());
@@ -55,9 +48,8 @@ std::map<std::string, std::string> get_rule(std::string file_name)
     return rule;
 }
 
-void change(std::map<std::string, std::string> rule, std::vector<std::string>& want_change_strings)
-{
-    for (auto &_s:want_change_strings) {
+void change(std::map<std::string, std::string> rule, std::vector<std::string> &want_change_strings) {
+    for (auto &_s : want_change_strings) {
         // 将 string _s 读入到input中
         std::string _temp_s = "", _res = "";
         std::stringstream input(_s);
@@ -80,24 +72,20 @@ void change(std::map<std::string, std::string> rule, std::vector<std::string>& w
     }
 }
 
-
-
-int main()
-{
+int main() {
     std::string rule_file_name = "./rule.txt";
     std::map<std::string, std::string> rule;
     rule = get_rule(rule_file_name);
-    for (auto _iter = rule.begin(); _iter != rule.end(); ++_iter)
-    {
+    std::cout << "输出rule规则配置：" << std::endl;
+    for (auto _iter = rule.begin(); _iter != rule.end(); ++_iter) {
         std::cout << _iter->first << ':' << _iter->second << std::endl;
     }
 
+    std::cout << "待转换文本：" << std::endl;
     std::string file_name = "./want_change_text.txt";
     std::vector<std::string> file_result;
-    if (read_file(file_name, file_result))
-    {
-        for (auto _s : file_result)
-        {
+    if (read_file(file_name, file_result)) {
+        for (auto _s : file_result) {
             std::cout << _s << std::endl;
         }
     }
--- Notebook/Algorithm/tree.cpp
+++ Notebook/Algorithm/tree.cpp
@@ -12,20 +12,21 @@ struct TreeNode {
 };
 
 class Solution {
-public:
-    TreeNode * createTree(std::vector<int> tree) {
+   public:
+    TreeNode *createTree(std::vector<int> tree) {
         TreeNode *root;
         return root;
     }
 
     // 前序遍历
     void preorder(TreeNode *root, std::vector<int> &res) {
-        if (root == nullptr) return;
+        if (root == nullptr)
+            return;
         res.push_back(root->val);
         preorder(root->left, res);
-        preorder(root->right,res);
+        preorder(root->right, res);
     }
-    std::vector<int> preorderTraversal(TreeNode* root) {
+    std::vector<int> preorderTraversal(TreeNode *root) {
         std::vector<int> res{};
         preorder(root, res);
         return res;
@@ -34,16 +35,14 @@ public:
     // 中序遍历
 
     // 后序遍历
-    
 };
 
-int main(int argc, char const *argv[])
-{
-    descriptions = [[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]]
+int main(int argc, char const *argv[]) {
+    std::vector<std::vector<int>> descriptions = {{20, 15, 1}, {20, 17, 0}, {50, 20, 1}, {50, 80, 0}, {80, 19, 1}};
     Solution s;
     std::vector<int> ans{};
     ans = s.preorderTraversal();
-    for (auto _v: ans) {
+    for (auto _v : ans) {
         std::cout << _v << ' ';
     }
     return 0;
